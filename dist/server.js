7189e52ee7c6976afe4fbdd228927fa98faa059e
Daniel Pokorny <danielp@kontent.ai>
2023-11-29 09:32:55 +0100

    initial commit

---
+++ dist/server.js
@@ -0,0 +1,63 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+require("dotenv/config");
+const body_parser_1 = __importDefault(require("body-parser"));
+// const onProxyRes: OnProxyResCallback = (proxyRes, req, res) => {
+//     let originalBody = new Buffer('');
+//     proxyRes.on('data', (data: Buffer) => {
+//       originalBody = Buffer.concat([originalBody, data]);
+//     });
+//     proxyRes.on('end', () => {
+//       const body = originalBody.toString('utf8');
+//       console.log('Response from Proxied Request:', body);
+//       // Be careful not to modify the response here
+//     });
+//   };
+const onDeliverProxyReq = (proxyReq, req, res) => {
+    // Add authorization header
+    const bearerToken = process.env.BEARER_TOKEN;
+    if (bearerToken) {
+        proxyReq.setHeader("Authorization", `Bearer ${bearerToken}`);
+    }
+    // Add body if there is a body in the incoming request
+    if (req.body) {
+        let bodyData = JSON.stringify(req.body);
+        proxyReq.setHeader("Content-Type", "application/json");
+        proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+        proxyReq.write(bodyData);
+    }
+};
+const onManageProxyReq = (proxyReq, req, res) => {
+    if (req.body) {
+        let bodyData = JSON.stringify(req.body);
+        proxyReq.setHeader("Content-Type", "application/json");
+        proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+        proxyReq.write(bodyData);
+    }
+};
+const deliverProxyOptions = {
+    target: "https://deliver.kontent.ai",
+    changeOrigin: true,
+    pathRewrite: { "^/api/deliver": "" },
+    onProxyReq: onDeliverProxyReq,
+};
+const manageProxyOptions = {
+    target: "https://manage.kontent.ai/v2", // Target for /api/manage
+    changeOrigin: true,
+    pathRewrite: { "^/api/manage": "" },
+    onProxyReq: onManageProxyReq,
+};
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json()); // for parsing application/json
+app.use(body_parser_1.default.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use("/api/deliver", (0, http_proxy_middleware_1.createProxyMiddleware)(deliverProxyOptions));
+app.use("/api/manage", (0, http_proxy_middleware_1.createProxyMiddleware)(manageProxyOptions));
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/server.ts
@@ -0,0 +1,71 @@
+import express, { request, response } from "express";
+import { createProxyMiddleware, Options } from "http-proxy-middleware";
+import "dotenv/config";
+import { OnProxyReqCallback, OnProxyResCallback } from "http-proxy-middleware/dist/types";
+import bodyParser from "body-parser";
+
+// const onProxyRes: OnProxyResCallback = (proxyRes, req, res) => {
+//     let originalBody = new Buffer('');
+  
+//     proxyRes.on('data', (data: Buffer) => {
+//       originalBody = Buffer.concat([originalBody, data]);
+//     });
+  
+//     proxyRes.on('end', () => {
+//       const body = originalBody.toString('utf8');
+//       console.log('Response from Proxied Request:', body);
+//       // Be careful not to modify the response here
+//     });
+//   };
+
+const onDeliverProxyReq: OnProxyReqCallback = (proxyReq, req, res) => {
+  // Add authorization header
+  const bearerToken = process.env.BEARER_TOKEN;
+  if (bearerToken) {
+    proxyReq.setHeader("Authorization", `Bearer ${bearerToken}`);
+  }
+
+  // Add body if there is a body in the incoming request
+  if (req.body) {
+    let bodyData = JSON.stringify(req.body);
+    proxyReq.setHeader("Content-Type", "application/json");
+    proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+    proxyReq.write(bodyData);
+  }
+};
+
+const onManageProxyReq: OnProxyReqCallback = (proxyReq, req, res) => {
+  if (req.body) {
+    let bodyData = JSON.stringify(req.body);
+    proxyReq.setHeader("Content-Type", "application/json");
+    proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+    proxyReq.write(bodyData);
+  }
+};
+
+const deliverProxyOptions: Options = {
+  target: "https://deliver.kontent.ai",
+  changeOrigin: true,
+  pathRewrite: { "^/api/deliver": "" },
+  onProxyReq: onDeliverProxyReq,
+};
+
+const manageProxyOptions: Options = {
+  target: "https://manage.kontent.ai/v2", // Target for /api/manage
+  changeOrigin: true,
+  pathRewrite: { "^/api/manage": "" },
+  onProxyReq: onManageProxyReq,
+};
+
+const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use("/api/deliver", createProxyMiddleware(deliverProxyOptions));
+app.use("/api/manage", createProxyMiddleware(manageProxyOptions));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
